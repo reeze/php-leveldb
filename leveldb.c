81302270cadb463a24bd78f0ed354aace76da9fd
Reeze Xia <reeze@php.net>
2012-09-12 01:27:58 +0800

    Dude I got @php.net now

--- leveldb.c
+++ leveldb.c
@@ -12,7 +12,7 @@
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
-  | Author: Reeze Xia <reeze.xia@gmail.com>                              |
+  | Author: Reeze Xia <reeze@php.net>                                    |
   +----------------------------------------------------------------------+
 */
 
