46c6f6c324ede4adc9f183f202677e7e37916535
Reeze Xia <reeze@php.net>
2012-11-02 00:20:32 +0800

    Use lastest leveldb: 1.7.0

--- travis/prepare.sh
+++ travis/prepare.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-wget http://leveldb.googlecode.com/files/leveldb-1.5.0.tar.gz
-tar zxvf leveldb-1.5.0.tar.gz
-cd leveldb-1.5.0 && make
+LDB_VERSION=1.7.0
+
+wget http://leveldb.googlecode.com/files/leveldb-$LDB_VERSION.tar.gz
+tar zxvf leveldb-$LDB_VERSION.tar.gz
+cd leveldb-$LDB_VERSION && make
 cd ..
-phpize && ./configure --with-leveldb=$PWD/leveldb-1.5.0 && make
+phpize && ./configure --with-leveldb=$PWD/leveldb-$LDB_VERSION && make
