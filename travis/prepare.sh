b880e0975d6a0c88c12f2667d953f410e5b7d9f9
Reeze Xia <reeze@php.net>
2013-02-24 15:25:29 +0800

    Fix variable name

--- travis/prepare.sh
+++ travis/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 wget http://leveldb.googlecode.com/files/leveldb-$LEVELDB_VERSION.tar.gz
-tar zxvf leveldb-$LDB_VERSION.tar.gz
-cd leveldb-$LDB_VERSION && make
+tar zxvf leveldb-$LEVELDB_VERSION.tar.gz
+cd leveldb-$LEVELDB_VERSION && make
 cd ..
 phpize && ./configure --with-leveldb=$PWD/leveldb-$LEVELDB_VERSION && make
