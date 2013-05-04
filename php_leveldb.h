8182e8f32b6db7bc6c6028bd368ee8bed61da0dc
Reeze Xia <reeze@php.net>
2013-05-04 17:12:41 +0800

    I need one more release

--- php_leveldb.h
+++ php_leveldb.h
@@ -21,7 +21,7 @@
 #ifndef PHP_LEVELDB_H
 #define PHP_LEVELDB_H
 
-#define PHP_LEVELDB_VERSION "0.2.0-dev"
+#define PHP_LEVELDB_VERSION "0.1.4-dev"
 
 extern zend_module_entry leveldb_module_entry;
 #define phpext_leveldb_ptr &leveldb_module_entry
