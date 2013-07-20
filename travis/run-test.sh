5c9b283b1a08e70920101782789cbd95c5506d09
Reeze Xia <reeze@php.net>
2013-07-20 20:13:01 +0800

    Disable Zend MM

--- travis/run-test.sh
+++ travis/run-test.sh
@@ -4,9 +4,11 @@ make install
 #make test
 
 export TEST_PHP_EXECUTABLE=`which php`
+export USE_ZEND_ALLOC=0
 
 php run-tests.php -d extension=leveldb.so -m tests/*.phpt
 
+# Display possible leaks and memory issues
 cat tests/*.mem
 
 # make test didn't return status code correctly
