70ca9c5635acbdcb657b0210a3847c66eee84a33
Dylan K. Taylor <odigiman@gmail.com>
2017-06-01 15:35:01 +0100

    Test against newer leveldb in Travis

--- travis/prepare.sh
+++ travis/prepare.sh
@@ -3,5 +3,8 @@
 curl -fsSL https://github.com/google/leveldb/archive/v$LEVELDB_VERSION.tar.gz -o leveldb-$LEVELDB_VERSION.tar.gz
 tar zxvf leveldb-$LEVELDB_VERSION.tar.gz
 cd leveldb-$LEVELDB_VERSION && make
+if [ "$SHLIB_DIR" != "" ]; then
+	cp $SHLIB_DIR/libleveldb.* .
+fi
 cd ..
 phpize && ./configure --with-leveldb=$PWD/leveldb-$LEVELDB_VERSION && make
