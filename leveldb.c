f858b3ea2b605c3aa3d61d2079401ae8b98a46b5
Dylan T <odigiman@gmail.com>
2021-02-11 21:16:15 +0000

    fix trying to free strings that are still in use elsewhere
    the logic in this code seems sus - it's possible it might be getting released too many times, or not enough. It needs closer investigation.

--- leveldb.c
+++ leveldb.c
@@ -171,7 +171,7 @@ void php_leveldb_object_free(zend_object *std)
 
 	if (obj->comparator) {
 		leveldb_comparator_destroy(obj->comparator);
-		zend_string_free(obj->callable_name);
+		zend_string_release(obj->callable_name);
 	}
 
 	zend_object_std_dtor(std);
@@ -996,7 +996,7 @@ PHP_METHOD(LevelDB, destroy)
 
 	if (comparator) {
 		leveldb_comparator_destroy(comparator);
-		zend_string_free(callable_name);
+		zend_string_release(callable_name);
 	}
 
 	leveldb_options_destroy(options);
@@ -1037,7 +1037,7 @@ PHP_METHOD(LevelDB, repair)
 
 	if (comparator) {
 		leveldb_comparator_destroy(comparator);
-		zend_string_free(callable_name);
+		zend_string_release(callable_name);
 	}
 
 	leveldb_options_destroy(options);
